caeda60bfa84d7514a1e4669ece05dbe72e02886
Guilherme Tchornei <guilhermetchornei@gmail.com>
2022-11-02 15:38:22 -0300

    criação de quizz: informacoes basicas

--- JS/script.js
+++ JS/script.js
@@ -1,6 +1,6 @@
 let listaQuizz = []; //Lista com todos os quizzes
 
-pegarQuizzes();
+//pegarQuizzes();
 
 function pegarQuizzes() {
     let promisse = axios.get("https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes");
@@ -17,4 +17,26 @@ function pegarQuizzes() {
 function renderizaQuizz(quizz) {
     //aqui entra o HTML
     console.log(quizz);
+}
+
+//  Criação de Quizz
+function informacoesBasicas() {
+    const input = document.querySelectorAll(".informacoes_basicas input");
+    if (input[0].value.length <= 20 || input[0].value.length >= 65)
+    {
+        alert("O título deve ter no mínimo 20 caracteres e no máximo 65");
+    }
+    else if (input[2].value < 3)
+    {
+        alert("O quizz deve ter pelo menos 3 perguntas");
+    }
+    else if (input[3].value < 2)
+    {
+        alert("O quizz deve ter pelo menos 2 níveis");
+    }
+    else
+    {
+        document.querySelector("informacoes_basicas").classList.add("escondido");
+        document.querySelector("perguntas").classList.remove("escondido");
+    }
 }
