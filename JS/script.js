347a8a1d6ef64af38690980a09305f2b76232c95
teuprimo <vinicius.brandaot@gmail.com>
2022-11-03 01:46:09 -0300

    template para quizz aberto

--- JS/script.js
+++ JS/script.js
@@ -1,7 +1,8 @@
 let listaQuizz = []; //Lista com todos os quizzes
 
+/*
 pegarQuizzes();
-
+*/
 function pegarQuizzes() {
     let promisse = axios.get("https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes");
     promisse.then((response) => {
@@ -18,11 +19,22 @@ function renderizaQuizz(quizz) {
     place = document.querySelector('.lista_quizz');
     console.log(place);
     place.innerHTML += `
-        <div class="quizz" id="">
+        <div class="quizz" id="" onclick='clicouQuizz(this)'>
         <div><img src="https://www.crunchyroll.com/imgsrv/display/thumbnail/1200x675/catalog/crunchyroll/e3f6c36d65ba484f80e778a2c12105be.jpeg" alt=""></div>
         <div class="nome_quizz"><h1>DESCUBRA SUA WAIFU! Qual a sua cara metade nos animes?</h1></div>
         </div>
     `;
 
     console.log(quizz);
+}
+
+function clicouQuizz(selecionado){
+
+    place = document.querySelector('main')
+
+    place.innerHTML = ``
+
+
+    console.log(selecionado);
+
 }
