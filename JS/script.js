2fb1c33e921dd169f5a1da767821003eff5cbda3
Guilherme Tchornei <guilhermetchornei@gmail.com>
2022-11-07 12:52:20 -0300

    exibição de quizz incompleto

--- JS/script.js
+++ JS/script.js
@@ -1,6 +1,7 @@
 let listaQuizz = []; //Lista com todos os quizzes
 let listaQuizzUsuario = recuperaDados();
 let place, erros, acertos;
+let contador, clicouAntes = [];
 
 
 pegarQuizzes();
@@ -47,7 +48,7 @@ function pegarQuizzes() {
 function renderizaQuizz(quizz, elemento) {
     //console.log(place);
     elemento.innerHTML += `
-        <div class="quizz" id="" onclick='clicouQuizz(this)'>
+        <div class="quizz" id="" onclick='exibirQuizz(${quizz.id})'>
         <div class="imagem" style="background-image: linear-gradient(to bottom, rgba(255,255,255,0), rgba(255,255,255,0), rgba(0,0,0,.5), rgba(0,0,0,.8)), url('${quizz.image}');"></div>
         <div class="nome_quizz">
         <h1>${quizz.title}</h1></div>
@@ -58,79 +59,156 @@ function renderizaQuizz(quizz, elemento) {
 }
 
 
-/* Ainda está como um template em html. precisa deixar bonitinho */
-function clicouQuizz(selecionado){
+
+function exibirQuizz(idQuizz){
+    const quizzSelecionado = listaQuizzUsuario.find((quizz) => { if (quizz.id === idQuizz) return quizz });
+
+    if (quizzSelecionado === null)
+        quizzSelecionado = listaQuizz.find((quizz) => { if (quizz.id === idQuizz) return quizz });
 
 
+
+    console.log(quizzSelecionado);
     place = document.querySelector('main');
     bannerPlace = document.querySelector('header');
-
     bannerPlace.innerHTML += `
-    <div class="banner">
+    <div class="banner" style="background: linear-gradient(0deg,
+        rgba(0, 0, 0, 0.57),
+        rgba(0, 0, 0, 0.57)), url(${quizzSelecionado.image});">
 
     </div>
     `;
+    let len = quizzSelecionado.questions.length;
 
     place.innerHTML = `
-    <div class="pergunta_caixa" id="">
+    <div class="pergunta_caixa pergunta0" id="pergunta0">
 
-    <div class="pergunta" id="">
-        <div class="pergunta_texto"><p>AQUI VAI A pergunta</p></div>
-    </div>
+            <div class="pergunta" id="" style="background-color:${quizzSelecionado.questions[0].color};">
+                <div class="pergunta_texto" ><p>${quizzSelecionado.questions[0].title}</p></div>
+            </div>
 
-    <div class="option_box">
-        <div class="option selected" id="">
-            <img src="https://static.anime21.blog.br/2021/04/11-4-1200x675-cropped.jpg" alt="">
-            <div class="option_name"><h4>questão</h4></div>
-        </div>
+        <div class="option_box" >
 
-        <div class="option unselected" id="">
-            <img src="https://www.comboinfinito.com.br/principal/wp-content/uploads/2020/07/Re-zero-rem-02-896x504-1.jpg" alt="">
-            <div class="option_name"><h4>questão</h4></div>
-        </div>
 
-        <div class="option unselected" id="">
-            <img src="https://www.comboinfinito.com.br/principal/wp-content/uploads/2020/07/Re-zero-rem-02-896x504-1.jpg" alt="">
-            <div class="option_name"><h4>questão</h4></div>
-        </div>
 
-        <div class="option unselected" id="">
-            <img src="https://www.comboinfinito.com.br/principal/wp-content/uploads/2020/07/Re-zero-rem-02-896x504-1.jpg" alt="">
-            <div class="option_name"><h4>questão</h4></div>
+
         </div>
     </div>
-</div>
 
 
-`;
+        `;
+
+    for(let j=1; j<len; j++){
+        place.innerHTML += `
+        <div class="pergunta_caixa " id="pergunta${j}">
+
+                <div class="pergunta" id="" style="background-color:${quizzSelecionado.questions[j].color};">
+                    <div class="pergunta_texto" ><p>${quizzSelecionado.questions[j].title}</p></div>
+                </div>
+
+            <div class="option_box" id=${j} >
 
 
-    console.log(selecionado);
-    resultado(place, acertos, erros);
+
+
+            </div>
+        </div>
+
+
+            `;
+    }
+
+        console.log("len" +len);
+        for(contador = 0; contador<len; contador++){
+            console.log("contador " + contador);
+            for(let i=0; i < quizzSelecionado.questions[contador].answers.length; i++){
+                opcoesQuizz(quizzSelecionado, i, contador);
+         }
+         perguntaNova(contador, place , quizzSelecionado);
+
+        }
+
+        resultado(place, acertos, erros, quizzSelecionado.levels);
+
+    }
+
+function opcoesQuizz(selecionado, i, aux){
+    console.log(aux + aux)
+    colocaPergunta = document.querySelector(`#pergunta${aux}`);
+    certo = selecionado.questions[aux].answers[i].isCorrectAnswer;
+    if(certo==true){
+        certo="correta";
+    }
+    else{
+        certo="incorreta";
+    }
+    console.log(certo);
+            colocaPergunta.innerHTML += `
+            <div class="option, opt${i}" id="${certo}" onclick="optionClick(this, ${aux})">
+                <img src="${selecionado.questions[contador].answers[i].image}" alt="">
+                <div class="option_name"><h4>${selecionado.questions[contador].answers[i].text}</h4></div>
+            `
+
+
+    colocaPergunta
+
 }
 
-function resultado(place, acertos, erros){
+function perguntaNova(contador, _place, selecionado){
+    console.log(selecionado)
+    console.log(contador)
+    _place += `
+    <div class="pergunta_caixa" id="">
+        <div class="pergunta" id="" style="background-color:${selecionado.questions[contador].color};">
+            <div class="pergunta_texto" ><p>${selecionado.questions[contador].title}</p></div>
+        </div>
+        <div class="option_box" id=${contador} >
+        </div>
+    </div>
+     `;
+}
+
+function optionClick(clicada, aux){
+    if(clicouAntes.includes(aux)){
+        return 0;
+    }
+    unselected = document.querySelector(`#pergunta${aux}`)
+    console.log(unselected);
+    unselected.querySelector(".opt0").classList.add("unselected");
+    unselected.querySelector(".opt1").classList.add("unselected");
+    unselected.querySelector(".opt2").classList.add("unselected");
+    unselected.querySelector(".opt3").classList.add("unselected");
+
+    clicada.classList.remove("unselected");
+    clicada.classList.add("selected");
+    clicouAntes.push(aux)
+
+}
+
+function resultado(place, acertos, erros, src){
     //inserir lógica de resultados
 
     let percentil = (erros/acertos)*100;
+
 
     place.innerHTML += `
     <div class="caixa_resultado">
 
         <div class="resultado_header">
-        <div>joooooooooooj</div>
+        <div>${src[0].title}</div>
         </div>
 
         <div class="container_row">
             <div class="resultado_img">
-            <img src="https://steamuserimages-a.akamaihd.net/ugc/495780761315593167/44E8B4F92DB9DC92646213D847F3B9F61C680C4D/?imw=1024&imh=576&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=true">
+            <img src="${src[0].image}">
             </div>
 
             <div class="resultado_text">
-            <p>ssssssssssss<p>
+            <p>${src[0].text}<p>
             </div>
         </div>
     </div>
+
 
     <button onclick="voltarHome()" class="voltar">     Voltar ao menu       </button>
     `
