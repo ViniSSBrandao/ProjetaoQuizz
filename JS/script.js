4406d3679b278d6a27a5738bb3122ceae5fde8d3
Vinicius Brandao <vinicius.brandaot@gmail.com>
2022-11-07 03:09:14 -0300

    EXIBICAO QUIZZ AO CLICAR

--- JS/script.js
+++ JS/script.js
@@ -1,5 +1,6 @@
 let listaQuizz = []; //Lista com todos os quizzes
 let place, erros, acertos;
+let contador;
 
 
 pegarQuizzes();
@@ -34,7 +35,7 @@ function renderizaQuizz(quizz) {
     place = document.querySelector('.lista_quizz');
     //console.log(place);
     place.innerHTML += `
-        <div class="quizz" id="" onclick='clicouQuizz(this)'>
+        <div class="quizz" id="${quizz.id}" onclick='clicouQuizz(${quizz.id})'>
         <div><img src="${quizz.image}" alt=""></div>
         <div class="nome_quizz"><h1>${quizz.title}</h1></div>
         </div>
@@ -46,53 +47,119 @@ function renderizaQuizz(quizz) {
 
 /* Ainda está como um template em html. precisa deixar bonitinho */
 function clicouQuizz(selecionado){
+    console.log(selecionado);
+    quizzSelecionado = axios.get(`https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes/${selecionado}`);
+    
+
+    quizzSelecionado.then(exibirQuizz);
+    /*quizzSelecionado.catch(alert("Erro! Tente selecionar novamente"))*/
+
+    
+    
+    
+}
 
 
+function exibirQuizz(selecionado){
+   
+    console.log(selecionado);
     place = document.querySelector('main');
     bannerPlace = document.querySelector('header');
-
+    id = selecionado.id;
+    console.log("awa "+id);
     bannerPlace.innerHTML += `
-    <div class="banner">
-
+    <div class="banner" style="background: linear-gradient(0deg,
+        rgba(0, 0, 0, 0.57),
+        rgba(0, 0, 0, 0.57)), url(${selecionado.data.image});">
+    
     </div>
     `;
+    console.log("tedio "+selecionado.data.image)
+    len = Object.keys(selecionado.data).length;
 
     place.innerHTML = `
-    <div class="pergunta_caixa" id="">
+    <div class="pergunta_caixa pergunta0" id="pergunta0">
+    
+            <div class="pergunta" id="" style="background-color:${selecionado.data.questions[0].color};">
+                <div class="pergunta_texto" ><p>${selecionado.data.questions[0].title}</p></div>
+            </div>
+            
+        <div class="option_box" >
+            
+        
+           
 
-    <div class="pergunta" id="">
-        <div class="pergunta_texto"><p>AQUI VAI A pergunta</p></div>
+        </div>    
     </div>
+        
+        
+        `;
 
-    <div class="option_box">
-        <div class="option selected" id="">
-            <img src="https://static.anime21.blog.br/2021/04/11-4-1200x675-cropped.jpg" alt="">
-            <div class="option_name"><h4>questão</h4></div>
+    for(let j=1; j<len; j++){
+        place.innerHTML += `
+        <div class="pergunta_caixa " id="pergunta${j}">
+        
+                <div class="pergunta" id="" style="background-color:${selecionado.data.questions[j].color};">
+                    <div class="pergunta_texto" ><p>${selecionado.data.questions[j].title}</p></div>
+                </div>
+                
+            <div class="option_box" id=${j} >
+                
+            
+            
+
+            </div>    
         </div>
+            
+            
+            `;
+    }
+        
+        console.log("len" +len);
+        for(contador = 0; contador<len; contador++){
+            console.log("contador " + contador);
+            for(let i=0; i<4; i++){
+                opcoesQuizz(selecionado, i, contador);
+         }
+         perguntaNova(contador, place , selecionado);
+        }
 
-        <div class="option unselected" id="">
-            <img src="https://www.comboinfinito.com.br/principal/wp-content/uploads/2020/07/Re-zero-rem-02-896x504-1.jpg" alt="">
-            <div class="option_name"><h4>questão</h4></div>
+        resultado(place, acertos, erros);
+    }
+    
+function opcoesQuizz(selecionado, i, aux){
+    console.log(aux + aux)
+    colocaPergunta = document.querySelector(`#pergunta${aux}`);
+    /*certo = selecionado.data.questions[contador].answers[i].isCorrectAnswer;
+    if(certo==true){
+        certo="correta";
+    }
+    else{
+        certo="incorreta";
+    }*/
+            colocaPergunta.innerHTML += `
+            <div class="option selected" id="${1}">
+                <img src="${selecionado.data.questions[contador].answers[i].image}" alt="">
+                <div class="option_name"><h4>${selecionado.data.questions[contador].answers[i].text}</h4></div>
+            `
+
+
+    colocaPergunta
+
+}
+
+function perguntaNova(contador, place, selecionado){
+    console.log(selecionado)
+    console.log(contador)
+    place += `
+    <div class="pergunta_caixa" id="">
+        <div class="pergunta" id="" style="background-color:${selecionado.data.questions[contador].color};">
+            <div class="pergunta_texto" ><p>${selecionado.data.questions[contador].title}</p></div>
         </div>
-
-        <div class="option unselected" id="">
-            <img src="https://www.comboinfinito.com.br/principal/wp-content/uploads/2020/07/Re-zero-rem-02-896x504-1.jpg" alt="">
-            <div class="option_name"><h4>questão</h4></div>
-        </div>
-
-        <div class="option unselected" id="">
-            <img src="https://www.comboinfinito.com.br/principal/wp-content/uploads/2020/07/Re-zero-rem-02-896x504-1.jpg" alt="">
-            <div class="option_name"><h4>questão</h4></div>
-        </div>
-    </div>    
-</div>
-
-
-`;
-
-
-    console.log(selecionado);
-    resultado(place, acertos, erros);
+        <div class="option_box" id=${contador} >   
+        </div>  
+    </div>
+     `;
 }
 
 function resultado(place, acertos, erros){
